6d38bcabaa0d7c18767441115e189183f3ce5a7c
Jędrzej Głowaczewski <jedrzej.glowaczewski@gmail.com>
2023-10-26 20:57:50 +0200

    Check image existence before downloading it

--- download-images.py
+++ download-images.py
@@ -3,20 +3,53 @@ import io
 import matplotlib
 import numpy as np
 import requests
+from dotenv import load_dotenv
 import os
 
 from shapely import Point, Polygon
 import matplotlib.pyplot as plt
 from PIL import Image
 
-API_KEY = "YOUR_API_KEY"
 IMAGE_SIZE = "256x256"
 OUTPUT_DIR = "./streetview_images/"
 
 
-def download_images(locations):
+def check_street_view_image_existence(api_key, location):
+    base_url = "https://maps.googleapis.com/maps/api/streetview/metadata"
+    params = {
+        "location": location,
+        "radius": 2000,  # Search within # meters of the location
+        "key": api_key
+    }
+
+    # Make the API request to retrieve metadata
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        metadata = response.json()
+
+        # Check the "status" field in the metadata
+        location = metadata.get("location")
+        if metadata.get("status") == "OK" and location:
+            lat = location.get("lat")
+            lng = location.get("lng")
+            if lat and lng:
+                return location.get("lat"), location.get("lng")
+            else:
+                print(f"ERROR: No latitude or longitude found in metadata. {metadata}")
+                return None, None  # Street View image does not exist
+        else:
+            print(f"ERROR: No image found. Status code {metadata.get('status')}")
+            return None, None  # Street View image does not exist
+    print(f"ERROR: Request failed. Status code {response.status_code}")
+    return None, None  # Request was not successful or failed
+
+
+def download_images(api_key, locations):
     for location in locations:
-        download_street_view_image(API_KEY, location, IMAGE_SIZE, output_dir=OUTPUT_DIR)
+        lat, lng = check_street_view_image_existence(api_key, f"{location[0]},{location[1]}")
+        if lat and lng:
+            download_street_view_image(api_key, (lat, lng), IMAGE_SIZE, output_dir=OUTPUT_DIR)
 
 
 def download_street_view_image(api_key, location, size, heading=0, pitch=0, fov=90, output_dir="./"):
@@ -96,6 +129,8 @@ def visualize_generated_locations(points, polygon):
 
 
 if __name__ == "__main__":
+    load_dotenv()
+    api_key = os.getenv("API_KEY")
     os.makedirs(OUTPUT_DIR, exist_ok=True)
     matplotlib.use('Agg')
 
@@ -124,4 +159,4 @@ if __name__ == "__main__":
     visualize_generated_locations(points_within_polygon, bounding_coordinates)
     print(points_within_polygon)
 
-    download_images(points_within_polygon)
+    download_images(api_key, points_within_polygon)
