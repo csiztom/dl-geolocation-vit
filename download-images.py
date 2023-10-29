a36b5511c730c330e6a99b29228d2253891e0211
Jędrzej Głowaczewski <jedrzej.glowaczewski@gmail.com>
2023-10-29 15:30:29 +0100

    Download images from a file

--- download-images.py
+++ download-images.py
@@ -1,26 +1,41 @@
 import io
+import json
 import time
 
 import matplotlib
-import numpy as np
 import requests
 from dotenv import load_dotenv
 import os
 
-from shapely import Point, Polygon
 import matplotlib.pyplot as plt
 from PIL import Image
 import concurrent.futures
 
-IMAGE_SIZE = "256x256"
+THREADS = 32
+IMAGE_SIZE = "224"
 OUTPUT_DIR = "./streetview_images/"
+LOCATIONS_FILE = "./data/locations.json"
+IMAGES_PER_LOCATION = 300
+# Set None to process all locations
+CHOSEN_LOCATIONS = [
+    "Randers Kommune",
+    "Silkeborg Kommune",
+    "Horsens Kommune",
+    "Gladsaxe Kommune",
+    "Roskilde Kommune",
+    "Kalundborg Kommune",
+    "Esbjerg Kommune",
+    "Kolding Kommune",
+    "Slagelse Kommune",
+    "Gentofte Kommune"
+]
 
 
 def check_street_view_image_existence(api_key, location):
     base_url = "https://maps.googleapis.com/maps/api/streetview/metadata"
     params = {
         "location": location,
-        "radius": 10000,  # Search within # meters of the location
+        "radius": 100,  # Search within # meters of the location
         "key": api_key
     }
 
@@ -47,15 +62,15 @@ def check_street_view_image_existence(api_key, location):
     return None, None  # Request was not successful or failed
 
 
-def download_images(api_key, locations):
-    with concurrent.futures.ThreadPoolExecutor(max_workers=32) as executor:
-        executor.map(verify_and_download_image, [api_key] * len(locations), locations)
+def download_images(api_key, locations, output_dir):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=THREADS) as executor:
+        executor.map(verify_and_download_image, [api_key] * len(locations), locations, [output_dir] * len(locations))
 
 
-def verify_and_download_image(api_key, location):
+def verify_and_download_image(api_key, location, output_dir):
     lat, lng = check_street_view_image_existence(api_key, f"{location[0]},{location[1]}")
     if lat and lng:
-        download_street_view_image(api_key, (lat, lng), IMAGE_SIZE, output_dir=OUTPUT_DIR)
+        download_street_view_image(api_key, (lat, lng), IMAGE_SIZE, output_dir=output_dir)
 
 
 def download_street_view_image(api_key, location, size, heading=0, pitch=0, fov=90, output_dir="./"):
@@ -77,51 +92,28 @@ def download_street_view_image(api_key, location, size, heading=0, pitch=0, fov=
     if response.status_code == 200:
         # Create a filename for the saved image
         filename = f"streetview_{location[0]}_{location[1]}.jpg"
-        filepath = output_dir + filename
+        filepath = os.path.join(output_dir, filename)
 
         # Save the image to the specified directory
         with open(filepath, "wb") as f:
             f.write(response.content)
-
-        print(f"Image downloaded and saved to {filepath}")
     else:
-        print("Image download failed.")
+        print("ERROR: Image download failed.")
 
 
-def generate_points_within_polygon(bounding_coordinates, num_points, precision=2):
-    # Create a Polygon object from the bounding coordinates
-    polygon = Polygon(bounding_coordinates)
+def visualize_generated_locations(points, polygon=None):
+    matplotlib.use('Agg')
 
-    # Calculate the bounding box of the polygon
-    min_x, min_y, max_x, max_y = polygon.bounds
-
-    points = []
-    while len(points) < num_points:
-        # Generate random points within the bounding box
-        random_point = Point(
-            round(np.random.uniform(min_x, max_x), precision),
-            round(np.random.uniform(min_y, max_y), precision)
-        )
-
-        # Check if the point is within the polygon
-        if random_point.within(polygon):
-            points.append((random_point.x, random_point.y))
-
-    return points
-
-
-def visualize_generated_locations(points, polygon):
     y = [point[0] for point in points]  # Extract the x coordinate from the tuple
     x = [point[1] for point in points]  # Extract the y coordinate from the tuple
-
-    # Extract x and y coordinates of the bounding polygon
-    y_polygon = [point[0] for point in polygon]
-    x_polygon = [point[1] for point in polygon]
 
     # Create a plot to visualize the points and the polygon
     plt.figure(figsize=(8, 6))
     plt.scatter(x, y, c='blue', label='Images locations')
-    plt.plot(x_polygon + [x_polygon[0]], y_polygon + [y_polygon[0]], c='red', label='Region')
+    if polygon:
+        y_polygon = [point[0] for point in polygon]
+        x_polygon = [point[1] for point in polygon]
+        plt.plot(x_polygon + [x_polygon[0]], y_polygon + [y_polygon[0]], c='red', label='Region')
     plt.xlabel('Longitude')
     plt.ylabel('Latitude')
     plt.title('Coordinates of images')
@@ -135,38 +127,67 @@ def visualize_generated_locations(points, polygon):
     Image.open(fig_io).show()
 
 
+def load_locations_from_file(file_path):
+    """
+    File format:
+    [
+        {
+            "location": [
+                9.0617879,
+                55.1878028
+            ],
+            "municipality": "T\u00f8nder Kommune"
+        },
+        {
+            "location": [
+                10.0307112,
+                55.9739344
+            ],
+            "municipality": "Odder Kommune"
+        },
+        ...
+    ]
+
+    Result:
+    [
+        "Kommune_Name": [
+            (56.156767293273234, 10.168922740310927),
+            (56.10563022646918, 10.168311008571386),
+        ]
+        ...
+    """
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    locations = {}
+    for item in data:
+        municipality = item['municipality']
+        if CHOSEN_LOCATIONS and municipality not in CHOSEN_LOCATIONS:
+            continue
+        location = tuple(item['location'])
+        if municipality in locations:
+            if len(locations[municipality]) >= IMAGES_PER_LOCATION:
+                continue
+            locations[municipality].append((location[1], location[0]))
+        else:
+            locations[municipality] = [(location[1], location[0])]
+    for location in locations:
+        print(f"{location}: {len(locations[location])} locations")
+    return locations
+
+
 if __name__ == "__main__":
     load_dotenv()
     api_key = os.getenv("API_KEY")
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
-    matplotlib.use('Agg')
 
-    bounding_coordinates = [
-        (57.71235801996257, 10.511603278278011),
-        (57.09299707300755, 8.563640295641799),
-        (54.948638340532014, 8.669805178100018),
-        (54.86653030666335, 9.966191651705254),
-        (55.42531058418789, 9.72449247866021),
-        (55.219034938904166, 9.955205325657753),
-        (55.09349647472793, 10.746220801077895),
-        (55.30667692080646, 10.768193453172898),
-        (55.31918142491171, 11.185673842977977),
-        (54.77791888196491, 11.031865278312948),
-        (55.58707567081864, 12.712773163580751),
-        (56.111297316227194, 12.251347469585669),
-        (55.406602591177425, 11.229619147167982),
-        (55.36916001966993, 10.779179779220401),
-        (55.61190363970056, 10.350713063367825),
-        (55.58086622333195, 9.867314717277736),
-        (56.440669583160854, 10.922002017837926),
-    ]
-    num_points = 1024
+    locations = load_locations_from_file('locations.json')
 
-    points_within_polygon = generate_points_within_polygon(bounding_coordinates, num_points)
-    visualize_generated_locations(points_within_polygon, bounding_coordinates)
-    print(points_within_polygon)
+    visualize_generated_locations([value for values in locations.values() for value in values])
 
     start = time.time()
-    download_images(api_key, points_within_polygon)
+    for municipality, locations in locations.items():
+        print(f"Downloading images for {municipality}...")
+        output_dir = os.path.join(OUTPUT_DIR, municipality)
+        os.makedirs(output_dir, exist_ok=True)
+        download_images(api_key, locations, output_dir)
     end = time.time()
-    print(f"Time elapsed: {end - start} seconds")
+    print(f"Images downloaded in {end - start} seconds")
