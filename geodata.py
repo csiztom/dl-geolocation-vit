7c89538a0351cf7b61a2aeb9e49eb724df5a864e
csiztom <tom.csiz@gmail.com>
2023-11-01 17:13:39 +0100

    geocells

--- geodata.py
+++ geodata.py
@@ -5,8 +5,14 @@ from turfpy.measurement import boolean_point_in_polygon
 with open("./data/denmark-adm7.geojson") as f:
     administrative = load(f)
 
-def municipality(lng, lat):
+cities = {}
+
+def municipality(lng, lat, city=None):
+    if city and city in cities:
+        return cities[city]
     for shape in administrative['features']:
         if boolean_point_in_polygon(Point((lng,lat)), shape['geometry']):
+            if city:
+                cities[city] = shape['properties']['local_name']
             return shape['properties']['local_name']
     return None
